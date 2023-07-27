3ff0e2c3ac8b1e45532aed9e39f0a4115cc15902
Kyle Nekto <nekto@veydlin.com>
2023-07-28 00:31:33 +0300

    add Extrapolation and Interpolation

--- Common/Utilities/Math/VMath/Utilities.h
+++ Common/Utilities/Math/VMath/Utilities.h
@@ -27,4 +27,16 @@ namespace VMath {
 	type Random(type a, type b) {
 		return a + rand() % (b - a + 1);
 	}
+
+
+	template<typename type>
+	type Extrapolation(type x0, type y0, type x1, type y1, type x2) {
+		return y0 + (((x2 - x0) * (y1 - y0)) / (x1 - x0));
+	}
+
+
+	template<typename type>
+	type Interpolation(type x0, type y0, type x1, type y1, type x2) {
+		return (((x2 - x0) * (y1 - y0)) / (x1 - x0)) + y0;
+	}
 };
