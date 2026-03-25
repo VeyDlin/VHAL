c44447f893c32ab0489d3efcb7cae621b8c46361
Kyle Nekto <nekto@veydlin.com>
2026-03-25 03:28:17 +0300

    fix: SetMin/SetMax in MathUtilities

--- Common/Utilities/Math/MathUtilities.h
+++ Common/Utilities/Math/MathUtilities.h
@@ -9,6 +9,20 @@ namespace MathUtilities {
 		return a + rand() % (b - a + 1);
 	}
 
+	template<typename type>
+	void SetMin(type &data, type val) {
+		if (val < data) {
+			data = val;
+		}
+	}
+
+	template<typename type>
+	void SetMax(type &data, type val) {
+		if (val > data) {
+			data = val;
+		}
+	}
+
 
 	template<typename type>
 	type Extrapolation(type x0, type y0, type x1, type y1, type x2) {
